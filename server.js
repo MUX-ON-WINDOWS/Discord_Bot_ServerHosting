6991572c093fc12aab4d9c33fc87da2bd593a488
Max <max.arnouts@ictmbo.nl>
2021-12-03 19:58:10 +0100

    Updated some statements

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ let Client = new Discord.Client();
 
 Client.on("message", message => {
   if (message.content === "Ping") {
-    message.channel.send("Kom dan kut wurm")
+    message.channel.send("Kom dan kut wurm");
   }
-
-})
-
-Client.on("message", message => {
-  if (message.content === "Ik hoek je") {
-    message.channel.send("( ͡° ͜ʖ ͡°)╭∩╮")
+  else if (message.content === "Ik hoek je" || message.content === "Ik sloop je") {
+    message.channel.send("( ͡° ͜ʖ ͡°)╭∩╮");
   }
-})
+  if (message.content === "Show your self") {
+    message.channel.send(
+      "https://cdn.glitch.me/5bc11192-3217-4ac2-a1ea-14bf684e28ec%2Fdisco_bot_profilepic.png?v=1638557028864"
+    );
+  }
+});
 
 
 Client.login("Your token")
