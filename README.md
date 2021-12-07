ac045ec9d3003b1bfe8869240141a156c6761daf
Max <max.arnouts@ictmbo.nl>
2021-12-07 17:21:03 +0100

    Updated the page and put the bot live

--- README.md
+++
@@ -1,5 +0,0 @@
-# discord_bot
-
-
-Use the command: npm i discord.js@12.5.3
-this will launch the 
---
+++ bot.js
@@ -0,0 +1,63 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const token = 'Your token';
+const keepAlive = require('./bot.js')
+keepAlive();
+// this the icon you need to put infront your command to let the command work
+const prefix = '/';
+
+// The bot will display Watching over MUX_ON_WINDOWS
+// Activity status (idle etc.)
+
+bot.on('ready', () => {
+  console.log(`${bot.user.tag} has logged in.`);
+  bot.user.setActivity('over MUX_ON_WINDOWS', {type: "WATCHING"})
+  .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+  .catch(console.error);
+});
+
+bot.on("message", message => {
+  if (message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ + /g);
+  const command = args.shift().toLocaleLowerCase();
+
+  if (command === 'info') {
+    const infoEmbed = new Discord.MessageEmbed()
+      .setTitle('MaxArnoutsDevelopment')
+      .setURL('https://maxarnouts.nl')
+      .setAuthor('Max Arnouts')
+      .setDescription('This is a test Description which we will see.')
+      .setColor('0xff0000')
+    try {
+      message.reply(infoEmbed);
+    } catch {
+      message.reply(`<@${message.author.username}> het lukt nu niet om de info op te halen!`)
+    }
+  }
+
+  if (command === "help") {
+    message.reply(`
+      Probeer deze commands te gebruiken:
+      - /face
+      - /projects
+      `)
+  } else
+  if (command === "face") {
+    message.channel.send("( ͡° ͜ʖ ͡°)╭∩╮");
+  }  else
+  // My projects 50/50
+  if (command === "projects"){
+    var d = Math.random();
+    if (d < 0.5){
+    // 50% chance of being here
+      message.channel.send("https://cdn.glitch.me/5bc11192-3217-4ac2-a1ea-14bf684e28ec%2Fhomepage_RS.png?v=1638559371704")
+    } else if (d < 1) {
+    // 50% chance of being here
+      message.channel.send("https://cdn.glitch.me/5bc11192-3217-4ac2-a1ea-14bf684e28ec%2FHomepage_KB.png?v=1638559366482")
+    }
+  }
+})
+
+bot.login(token);
+
