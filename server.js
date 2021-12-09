11709fefaaed4832b9ecfb3b70c4838b629fc93e
Max Arnouts <71127049+MUX-ON-WINDOWS@users.noreply.github.com>
2021-12-09 14:07:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const server = express();
+const name = "Muxic#0529";
 
 server.all('/', (req, res) => {
-    res.send('Your bot is online')
+  res.send(`${name} has logged in.`)
 })
 
 function keepAllive() {
-    server.listen(3000, () => {
-        console.log("Server is ready" + Date.now())
-    });
+  server.listen(3000, () => {
+    console.log("Server is ready" + Date.now())
+  });
 }
 
 module.exports = keepAllive;
+
